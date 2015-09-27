736ccbb316cb81e104e77e4a568b8c14d515b5b9
Cortland Walker <cortland1568@gmail.com>
2015-09-26 21:17:50 -0500

    Create gameworld and gamerenderer classes

---
+++ core/src/com/kilobolt/gameworld/GameRenderer.java
@@ -0,0 +1,6 @@
+package com.kilobolt.gameworld;
+
+public class GameRenderer {
+	// When GameScreen is created, we must create a
+	// new GameWorld object and a new GameRenderer object.
+}
---
+++ core/src/com/kilobolt/gameworld/GameWorld.java
@@ -0,0 +1,6 @@
+package com.kilobolt.gameworld;
+
+public class GameWorld {
+	// When GameScreen is created, we must create a
+	// new GameWorld object and a new GameRenderer object.
+}
