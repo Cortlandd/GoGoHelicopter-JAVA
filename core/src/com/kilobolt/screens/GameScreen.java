d38796ea47dabe21298219b45f1e0803295110b7
Cortland Walker <cortland1568@gmail.com>
2015-09-26 21:16:54 -0500

    Comment over what will likely be the game loop

--- core/src/com/kilobolt/screens/GameScreen.java
+++ core/src/com/kilobolt/screens/GameScreen.java
@@ -15,12 +15,15 @@ public class GameScreen implements Screen {
 		Gdx.app.log("GameScreen", "show called");
 	}
 
+	// Will basically be the game loop
 	@Override
 	public void render(float delta) {
 		// Sets a Color to Fill the Screen with (RGB = 10, 15, 230), Opacity of 1 (100%)
-        Gdx.gl.glClearColor(10/255.0f, 15/255.0f, 230/255.0f, 1f);
-        // Fills the screen with the selected color
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	    Gdx.gl.glClearColor(10/255.0f, 15/255.0f, 230/255.0f, 1f);
+	    // Fills the screen with the selected color
+	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	    // Covert Frame rate to String, print it
+	    Gdx.app.log("GameScreen FPS", (1/delta) + "");
 	}
 
 	@Override
