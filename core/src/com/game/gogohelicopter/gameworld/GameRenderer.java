83010638d48fa7c1a86d14fe544621975c0df751
Cortland Walker <cortland1568@gmail.com>
2015-09-28 00:42:17 -0500

    Added Sprite to GameRenderer

--- core/src/com/game/gogohelicopter/gameworld/GameRenderer.java
+++ core/src/com/game/gogohelicopter/gameworld/GameRenderer.java
@@ -6,24 +6,31 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameRenderer {
 	
 	private GameWorld myWorld;
     private OrthographicCamera cam;
     private ShapeRenderer shapeRenderer;
+    
+    private SpriteBatch batcher;
 	
 	public GameRenderer(GameWorld world) {
 		myWorld = world;
         cam = new OrthographicCamera();
         cam.setToOrtho(true, 136, 204);
+        
+        batcher = new SpriteBatch();
+        // Attach to camera
+        batcher.setProjectionMatrix(cam.combined);
+        
         shapeRenderer = new ShapeRenderer();
         shapeRenderer.setProjectionMatrix(cam.combined);
 	}
 	
 	public void render() {
-		Gdx.app.log("GameRenderer", "render");
-		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
 }
