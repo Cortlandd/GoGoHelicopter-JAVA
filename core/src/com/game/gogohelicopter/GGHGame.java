c4e01ca608d04eb60622d3428dd05b96ebdaa06e
Cortland Walker <cortland1568@gmail.com>
2015-09-28 00:35:22 -0500

    Setup AssetLoader

--- core/src/com/game/gogohelicopter/GGHGame.java
+++ core/src/com/game/gogohelicopter/GGHGame.java
@@ -2,6 +2,7 @@ package com.game.gogohelicopter;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.game.gogohelicopter.gghhelpers.AssetLoader;
 import com.game.gogohelicopter.screens.GameScreen;
 
 public class GGHGame extends Game {
@@ -12,9 +13,18 @@ public class GGHGame extends Game {
 		// implemented specifically for each platform.
 		Gdx.app.log("GGHGame", "created");
 		
+		// So that we can load all the images before the GameScreen is created.
+		AssetLoader.load();
+		
 		// Set this class to the screen created under
 		// com.kilobolt.screens/GameScreen.java
 		setScreen(new GameScreen());
+	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();
+		AssetLoader.dispose();
 	}
 	
 }
