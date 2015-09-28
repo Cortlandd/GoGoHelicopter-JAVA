effaa4db64bbd082ee5126a0c2267767fcd69834
Cortland Walker <cortland1568@gmail.com>
2015-09-28 15:09:02 -0500

    Added helicopter Rotation

--- core/src/com/game/gogohelicopter/objects/Helicopter.java
+++ core/src/com/game/gogohelicopter/objects/Helicopter.java
@@ -48,14 +48,42 @@ public class Helicopter {
 	
 	// Called when GameWorld.class updates
 	public void update(float delta) {
-		velocity.add(acceleration.cpy().scl(delta));
+
+        velocity.add(acceleration.cpy().scl(delta));
 
         if (velocity.y > 200) {
             velocity.y = 200;
         }
 
         position.add(velocity.cpy().scl(delta));
-	}
+
+        // Rotate counterclockwise
+        if (velocity.y < 0) {
+            rotation -= 600 * delta;
+
+            if (rotation < -20) {
+                rotation = -20;
+            }
+        }
+
+        // Rotate clockwise
+        if (isFalling()) {
+            rotation += 480 * delta;
+            if (rotation > 90) {
+                rotation = 90;
+            }
+
+        }
+
+    }
+
+    public boolean isFalling() {
+        return velocity.y > 110;
+    }
+
+    public boolean shouldntFlap() {
+        return velocity.y > 70;
+    }
 	
 	public void onClick() {
         velocity.y = -140;
