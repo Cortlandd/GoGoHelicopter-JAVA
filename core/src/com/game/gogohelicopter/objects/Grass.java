1c21607eef0b577ff1a39334eea020e87171f504
Cortland Walker <cortland1568@gmail.com>
2015-09-28 15:38:46 -0500

    Created pipe, grass, and scrolling handling for both

---
+++ core/src/com/game/gogohelicopter/objects/Grass.java
@@ -0,0 +1,12 @@
+package com.game.gogohelicopter.objects;
+
+public class Grass extends Scrollable {
+
+    // When Grass's constructor is invoked, invoke the super (Scrollable)
+    // constructor
+    public Grass(float x, float y, int width, int height, float scrollSpeed) {
+        super(x, y, width, height, scrollSpeed);
+
+    }
+
+}
---
+++ core/src/com/game/gogohelicopter/objects/ScrollHandler.java
@@ -0,0 +1,69 @@
+package com.game.gogohelicopter.objects;
+
+public class ScrollHandler {
+
+    private Grass frontGrass, backGrass;
+    private Pipe pipe1, pipe2, pipe3;
+    public static final int SCROLL_SPEED = -59;
+    public static final int PIPE_GAP = 49;
+
+    public ScrollHandler(float yPos) {
+        frontGrass = new Grass(0, yPos, 143, 11, SCROLL_SPEED);
+        backGrass = new Grass(frontGrass.getTailX(), yPos, 143, 11,
+                SCROLL_SPEED);
+
+        pipe1 = new Pipe(210, 0, 22, 60, SCROLL_SPEED);
+        pipe2 = new Pipe(pipe1.getTailX() + PIPE_GAP, 0, 22, 70, SCROLL_SPEED);
+        pipe3 = new Pipe(pipe2.getTailX() + PIPE_GAP, 0, 22, 60, SCROLL_SPEED);
+    }
+
+    public void update(float delta) {
+        // Update our objects
+        frontGrass.update(delta);
+        backGrass.update(delta);
+        pipe1.update(delta);
+        pipe2.update(delta);
+        pipe3.update(delta);
+
+        // Check if any of the pipes are scrolled left,
+        // and reset accordingly
+        if (pipe1.isScrolledLeft()) {
+            pipe1.reset(pipe3.getTailX() + PIPE_GAP);
+        } else if (pipe2.isScrolledLeft()) {
+            pipe2.reset(pipe1.getTailX() + PIPE_GAP);
+
+        } else if (pipe3.isScrolledLeft()) {
+            pipe3.reset(pipe2.getTailX() + PIPE_GAP);
+        }
+
+        // Same with grass
+        if (frontGrass.isScrolledLeft()) {
+            frontGrass.reset(backGrass.getTailX());
+
+        } else if (backGrass.isScrolledLeft()) {
+            backGrass.reset(frontGrass.getTailX());
+
+        }
+    }
+
+    public Grass getFrontGrass() {
+        return frontGrass;
+    }
+
+    public Grass getBackGrass() {
+        return backGrass;
+    }
+
+    public Pipe getPipe1() {
+        return pipe1;
+    }
+
+    public Pipe getPipe2() {
+        return pipe2;
+    }
+
+    public Pipe getPipe3() {
+        return pipe3;
+    }
+
+}
