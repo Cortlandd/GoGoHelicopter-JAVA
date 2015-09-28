c8bba12f173a52a32f1543a3dbc72f81de3fe548
Cortland Walker <cortland1568@gmail.com>
2015-09-28 15:50:14 -0500

    Updated scroll speed and rendered the pipe and grass(sidewalk)

--- core/src/com/game/gogohelicopter/gameworld/GameRenderer.java
+++ core/src/com/game/gogohelicopter/gameworld/GameRenderer.java
@@ -11,7 +11,10 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.game.gogohelicopter.gghhelpers.AssetLoader;
+import com.game.gogohelicopter.objects.Grass;
 import com.game.gogohelicopter.objects.Helicopter;
+import com.game.gogohelicopter.objects.Pipe;
+import com.game.gogohelicopter.objects.ScrollHandler;
 
 public class GameRenderer {
 	
@@ -26,6 +29,9 @@ public class GameRenderer {
     
     // Game Objects
     private Helicopter helicopter;
+    private ScrollHandler scroller;
+    private Grass frontGrass, backGrass;
+    private Pipe pipe1, pipe2, pipe3;
     
     // Game Assets
     private TextureRegion bg, grass;
@@ -58,7 +64,7 @@ public class GameRenderer {
 	
 	public void render(float runTime) {
 
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         shapeRenderer.begin(ShapeType.Filled);
@@ -81,7 +87,15 @@ public class GameRenderer {
         batcher.disableBlending();
         batcher.draw(bg, 0, midPointY + 23, 136, 43);
 
+        // 1. Draw Grass
+        drawGrass();
+
+        // 2. Draw Pipes
+        drawPipes();
         batcher.enableBlending();
+
+        // 3. Draw Skulls (requires transparency)
+        drawSkulls();
 
         if (helicopter.shouldntFlap()) {
             batcher.draw(helicopterMid, helicopter.getX(), helicopter.getY(),
@@ -101,6 +115,12 @@ public class GameRenderer {
 
     private void initGameObjects() {
         helicopter = myWorld.getHelicopter();
+        scroller = myWorld.getScroller();
+        frontGrass = scroller.getFrontGrass();
+        backGrass = scroller.getBackGrass();
+        pipe1 = scroller.getPipe1();
+        pipe2 = scroller.getPipe2();
+        pipe3 = scroller.getPipe3();  
     }
 
     private void initAssets() {
@@ -113,5 +133,52 @@ public class GameRenderer {
         skullUp = AssetLoader.skullUp;
         skullDown = AssetLoader.skullDown;
         bar = AssetLoader.bar;
+    }
+    
+    private void drawGrass() {
+        // Draw the grass
+        batcher.draw(grass, frontGrass.getX(), frontGrass.getY(),
+                frontGrass.getWidth(), frontGrass.getHeight());
+        batcher.draw(grass, backGrass.getX(), backGrass.getY(),
+                backGrass.getWidth(), backGrass.getHeight());
+    }
+
+    private void drawSkulls() {
+        // Temporary code! Sorry about the mess :)
+        // We will fix this when we finish the Pipe class.
+
+        batcher.draw(skullUp, pipe1.getX() - 1,
+                pipe1.getY() + pipe1.getHeight() - 14, 24, 14);
+        batcher.draw(skullDown, pipe1.getX() - 1,
+                pipe1.getY() + pipe1.getHeight() + 45, 24, 14);
+
+        batcher.draw(skullUp, pipe2.getX() - 1,
+                pipe2.getY() + pipe2.getHeight() - 14, 24, 14);
+        batcher.draw(skullDown, pipe2.getX() - 1,
+                pipe2.getY() + pipe2.getHeight() + 45, 24, 14);
+
+        batcher.draw(skullUp, pipe3.getX() - 1,
+                pipe3.getY() + pipe3.getHeight() - 14, 24, 14);
+        batcher.draw(skullDown, pipe3.getX() - 1,
+                pipe3.getY() + pipe3.getHeight() + 45, 24, 14);
+    }
+
+    private void drawPipes() {
+        // Temporary code! Sorry about the mess :)
+        // We will fix this when we finish the Pipe class.
+        batcher.draw(bar, pipe1.getX(), pipe1.getY(), pipe1.getWidth(),
+                pipe1.getHeight());
+        batcher.draw(bar, pipe1.getX(), pipe1.getY() + pipe1.getHeight() + 45,
+                pipe1.getWidth(), midPointY + 66 - (pipe1.getHeight() + 45));
+
+        batcher.draw(bar, pipe2.getX(), pipe2.getY(), pipe2.getWidth(),
+                pipe2.getHeight());
+        batcher.draw(bar, pipe2.getX(), pipe2.getY() + pipe2.getHeight() + 45,
+                pipe2.getWidth(), midPointY + 66 - (pipe2.getHeight() + 45));
+
+        batcher.draw(bar, pipe3.getX(), pipe3.getY(), pipe3.getWidth(),
+                pipe3.getHeight());
+        batcher.draw(bar, pipe3.getX(), pipe3.getY() + pipe3.getHeight() + 45,
+                pipe3.getWidth(), midPointY + 66 - (pipe3.getHeight() + 45));
     }
 }
--- core/src/com/game/gogohelicopter/objects/ScrollHandler.java
+++ core/src/com/game/gogohelicopter/objects/ScrollHandler.java
@@ -4,7 +4,7 @@ public class ScrollHandler {
 
     private Grass frontGrass, backGrass;
     private Pipe pipe1, pipe2, pipe3;
-    public static final int SCROLL_SPEED = -59;
+    public static final int SCROLL_SPEED = -80;
     public static final int PIPE_GAP = 49;
 
     public ScrollHandler(float yPos) {
