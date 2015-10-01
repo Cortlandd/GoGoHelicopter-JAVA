0dbcbde887084fd3d9b0432914e0d9635ee02a5c
John DeSilva <desilvjo@umich.edu>
2015-10-01 15:01:49 -0400

    Implement Android AdInterface

--- android/src/com/game/gogohelicopter/android/AndroidLauncher.java
+++ android/src/com/game/gogohelicopter/android/AndroidLauncher.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.game.gogohelicopter.GGHGame;
 import com.game.gogohelicopter.AdInterface;
+import com.saturnup.sdk.Saturnup;
+import com.saturnup.sdk.question.*;
 
 public class AndroidLauncher extends AndroidApplication implements AdInterface {
 	@Override
@@ -13,11 +15,19 @@ public class AndroidLauncher extends AndroidApplication implements AdInterface {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new GGHGame(), config);
+		Saturnup.initialize(this, "API_KEY");
 	}
 
     @Override
     public void show(final AdInterface.DoneCallback callback) {
-        // Unimplemented
-        callback.done();
+        Saturnup
+            .questionAd(this)
+            .listener(new QuestionAdListener() {
+                @Override
+                public void onResult(QuestionAdResult result) {
+                    callback.done();
+                }
+            })
+        .show();
     }
 }
