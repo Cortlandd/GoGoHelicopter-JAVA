c9faddd577122d8661c5a0fa5cc03d683966d840
John DeSilva <desilvjo@umich.edu>
2015-10-01 15:00:45 -0400

    Add AdInterface to show native ads on each platform

--- android/src/com/game/gogohelicopter/android/AndroidLauncher.java
+++ android/src/com/game/gogohelicopter/android/AndroidLauncher.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.game.gogohelicopter.GGHGame;
+import com.game.gogohelicopter.AdInterface;
 
-public class AndroidLauncher extends AndroidApplication {
+public class AndroidLauncher extends AndroidApplication implements AdInterface {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new GGHGame(), config);
 	}
+
+    @Override
+    public void show(final AdInterface.DoneCallback callback) {
+        // Unimplemented
+        callback.done();
+    }
 }
--- desktop/src/com/game/gogohelicopter/desktop/DesktopLauncher.java
+++ desktop/src/com/game/gogohelicopter/desktop/DesktopLauncher.java
@@ -3,8 +3,9 @@ package com.game.gogohelicopter.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.game.gogohelicopter.GGHGame;
+import com.game.gogohelicopter.AdInterface;
 
-public class DesktopLauncher {
+public class DesktopLauncher implements AdInterface {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Go Go Helicopter";
@@ -12,4 +13,10 @@ public class DesktopLauncher {
         config.height = 408;
 		new LwjglApplication(new GGHGame(), config);
 	}
+
+    @Override
+    public void showAd(AdInterface.DoneCallback callback) {
+        // Unimplemented
+        callback.done();
+    }
 }
